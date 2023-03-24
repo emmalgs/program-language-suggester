ef9b0f5c64db82a5c1030f93d18df6b1566fcc33
Emma Gerigscott <elgerig@gmail.com>
2023-03-24 09:26:40 -0700

    WIP: add more options for C#, create seperate function for language display

--- js/scripts.js
+++ js/scripts.js
@@ -12,12 +12,23 @@ function langPicker(veg, hat, bug) {
     const resultLinks = document.querySelector("#links")
     const links = document.createElement("a")
 
-    if (veg === "corn" && hat === "fedora" && bug === "beetle") {
+    if ((veg === "corn" && hat === "fedora" && bug === "beetle") ||
+        (veg === "carrot" && hat === "cap" && bug === "bee") ||
+        (veg === "lettuce" &&)) {
         resultLang.innerText = "C#"
         links.setAttribute("href", "https://exercism.org/tracks/csharp")
         links.innerText = "Exercism.org"
         resultLinks.append(links)
-    } else if ()
+    } else if (veg === "")
+}
+
+function langInfo(lang) {
+    if (lang === "C#") {
+        resultLang.innerText = "C#"
+        links.setAttribute("href", "https://exercism.org/tracks/csharp")
+        links.innerText = "Exercism.org"
+        resultLinks.append(links)
+    }
 }
 
 window.addEventListener("load", () => {
