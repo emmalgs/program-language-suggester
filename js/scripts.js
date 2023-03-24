9fbe4bb1007dfec38d737890ccb1b2d20637c1f3
Emma Gerigscott <elgerig@gmail.com>
2023-03-24 09:21:41 -0700

    create userinput function, fires onload, and runs first check

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function userInput(e) {
+    e.preventDefault();
+    const veg = document.querySelector("input[name='veg']:checked").value;
+    const hat = document.querySelector("input[name='hat']:checked").value;
+    const bug = document.querySelector("input[name='bug']:checked").value;
+    console.log(veg)
+    langPicker(veg, hat, bug)
+}
+
+function langPicker(veg, hat, bug) {
+    const resultLang = document.querySelector("#lang")
+    const resultLinks = document.querySelector("#links")
+    const links = document.createElement("a")
+
+    if (veg === "corn" && hat === "fedora" && bug === "beetle") {
+        resultLang.innerText = "C#"
+        links.setAttribute("href", "https://exercism.org/tracks/csharp")
+        links.innerText = "Exercism.org"
+        resultLinks.append(links)
+    } else if ()
+}
+
+window.addEventListener("load", () => {
+    document.querySelector("form").addEventListener("submit", userInput)
+})
